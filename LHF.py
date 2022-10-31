3337ff72c11ed1ad67bd12db65d633540514773e
Lotosik <91878912+SkillsAngels@users.noreply.github.com>
2022-10-31 18:06:41 +0300

    Add files via upload

---
+++ LHF.py
@@ -0,0 +1,25 @@
+from .. import loader
+
+class LHF(Loader.Library):
+    developer = "@lotosiiiik"
+    version = (0, 0, 1)
+    
+    async def message_q(
+        self,
+        text: str,
+        user_id: int,
+        mark_read: bool = False,
+        delete: bool = False,
+    ):
+        """Отправляет сообщение и возращает ответ"""
+        async with self.client.conversation(user_id) as conv:
+            msg = await conv.send_message(text)
+            response = await conv.get_response()
+            if mark_read:
+                await conv.mark_read()
+
+            if delete:
+                await msg.delete()
+                await response.delete()
+
+            return response
